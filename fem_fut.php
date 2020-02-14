d7a4d79d39339761d23c5c6d8b5d1d743041290c
Hugo Cobos <hugocobos97@hotmail.com>
2020-02-13 21:14:07 -0600

    ya realiza el primer ingreso de datos, solo tengo que encontrar la forma de corregir un error porque si hay espacios en blancos aún asi rellena el campo de 'tecnologico'

---
+++ fem_fut.php
@@ -0,0 +1,290 @@
+<?php
+
+NuevoProducto($_POST['tecnologico'],$_POST['rfc'], $_POST['nombre'],$_POST['apellidos'],$_POST['documento']);
+
+function NuevoProducto($tecnologico,$rfc,$nombre,$apellidos,$documento)
+{
+
+    include 'conexion.php';
+
+    $sentencia= "INSERT INTO `fut_femenil` (`tecnologico`,`rfc_ff`, `nombre`, `apellidos`, `adjunto_ff`) 
+    VALUES ('".$tecnologico."','".$rfc."','".$nombre."','".$apellidos."','".$documento."') ";
+    $conexion->query($sentencia) or die ("Error al ingresar los datos".mysqli_error($conexion));
+
+}
+
+NuevoProducto1($_POST['tecnologico'],$_POST['rfc1'], $_POST['nombre1'],$_POST['apellidos1'],$_POST['documento1']);
+
+function NuevoProducto1($tecnologico,$rfc1,$nombre1,$apellidos1,$documento1)
+{
+
+    include 'conexion.php';
+
+    $sentencia= "INSERT INTO `fut_femenil` (`tecnologico`,`rfc_ff`, `nombre`, `apellidos`, `adjunto_ff`) 
+    VALUES ('".$tecnologico."','".$rfc1."','".$nombre1."','".$apellidos1."','".$documento1."') ";
+    $conexion->query($sentencia) or die ("Error al ingresar los datos".mysqli_error($conexion));
+
+}
+
+NuevoProducto2($_POST['tecnologico'],$_POST['rfc2'], $_POST['nombre2'],$_POST['apellidos2'],$_POST['documento2']);
+
+function NuevoProducto2($tecnologico,$rfc2,$nombre2,$apellidos2,$documento2)
+{
+
+    include 'conexion.php';
+
+    $sentencia= "INSERT INTO `fut_femenil` (`tecnologico`,`rfc_ff`, `nombre`, `apellidos`, `adjunto_ff`) 
+    VALUES ('".$tecnologico."','".$rfc2."','".$nombre2."','".$apellidos2."','".$documento2."') ";
+    $conexion->query($sentencia) or die ("Error al ingresar los datos".mysqli_error($conexion));
+
+}
+
+NuevoProducto3($_POST['tecnologico'],$_POST['rfc3'], $_POST['nombre3'],$_POST['apellidos3'],$_POST['documento3']);
+
+function NuevoProducto3($tecnologico,$rfc3,$nombre3,$apellidos3,$documento3)
+{
+
+    include 'conexion.php';
+
+    $sentencia= "INSERT INTO `fut_femenil` (`tecnologico`,`rfc_ff`, `nombre`, `apellidos`, `adjunto_ff`) 
+    VALUES ('".$tecnologico."','".$rfc3."','".$nombre3."','".$apellidos3."','".$documento3."') ";
+    $conexion->query($sentencia) or die ("Error al ingresar los datos".mysqli_error($conexion));
+
+}
+
+NuevoProducto4($_POST['tecnologico'],$_POST['rfc4'], $_POST['nombre4'],$_POST['apellidos4'],$_POST['documento4']);
+
+function NuevoProducto4($tecnologico,$rfc4,$nombre4,$apellidos4,$documento4)
+{
+
+    include 'conexion.php';
+
+    $sentencia= "INSERT INTO `fut_femenil` (`tecnologico`,`rfc_ff`, `nombre`, `apellidos`, `adjunto_ff`) 
+    VALUES ('".$tecnologico."','".$rfc4."','".$nombre4."','".$apellidos4."','".$documento4."') ";
+    $conexion->query($sentencia) or die ("Error al ingresar los datos".mysqli_error($conexion));
+
+}
+
+NuevoProducto5($_POST['tecnologico'],$_POST['rfc5'], $_POST['nombre5'],$_POST['apellidos5'],$_POST['documento5']);
+
+function NuevoProducto5($tecnologico,$rfc5,$nombre5,$apellidos5,$documento5)
+{
+
+    include 'conexion.php';
+
+    $sentencia= "INSERT INTO `fut_femenil` (`tecnologico`,`rfc_ff`, `nombre`, `apellidos`, `adjunto_ff`) 
+    VALUES ('".$tecnologico."','".$rfc5."','".$nombre5."','".$apellidos5."','".$documento5."') ";
+    $conexion->query($sentencia) or die ("Error al ingresar los datos".mysqli_error($conexion));
+
+}
+NuevoProducto6($_POST['tecnologico'],$_POST['rfc6'], $_POST['nombre6'],$_POST['apellidos6'],$_POST['documento6']);
+
+function NuevoProducto6($tecnologico,$rfc6,$nombre6,$apellidos6,$documento6)
+{
+
+    include 'conexion.php';
+
+    $sentencia= "INSERT INTO `fut_femenil` (`tecnologico`,`rfc_ff`, `nombre`, `apellidos`, `adjunto_ff`) 
+    VALUES ('".$tecnologico."','".$rfc6."','".$nombre6."','".$apellidos6."','".$documento6."') ";
+    $conexion->query($sentencia) or die ("Error al ingresar los datos".mysqli_error($conexion));
+
+}
+NuevoProducto7($_POST['tecnologico'],$_POST['rfc7'], $_POST['nombre7'],$_POST['apellidos7'],$_POST['documento7']);
+
+function NuevoProducto7($tecnologico,$rfc7,$nombre7,$apellidos7,$documento7)
+{
+
+    include 'conexion.php';
+
+    $sentencia= "INSERT INTO `fut_femenil` (`tecnologico`,`rfc_ff`, `nombre`, `apellidos`, `adjunto_ff`) 
+    VALUES ('".$tecnologico."','".$rfc7."','".$nombre7."','".$apellidos7."','".$documento7."') ";
+    $conexion->query($sentencia) or die ("Error al ingresar los datos".mysqli_error($conexion));
+
+}
+NuevoProducto8($_POST['tecnologico'],$_POST['rfc8'], $_POST['nombre8'],$_POST['apellidos8'],$_POST['documento8']);
+
+function NuevoProducto8($tecnologico,$rfc8,$nombre8,$apellidos8,$documento8)
+{
+
+    include 'conexion.php';
+
+    $sentencia= "INSERT INTO `fut_femenil` (`tecnologico`,`rfc_ff`, `nombre`, `apellidos`, `adjunto_ff`) 
+    VALUES ('".$tecnologico."','".$rfc8."','".$nombre8."','".$apellidos8."','".$documento8."') ";
+    $conexion->query($sentencia) or die ("Error al ingresar los datos".mysqli_error($conexion));
+
+}
+NuevoProducto9($_POST['tecnologico'],$_POST['rfc9'], $_POST['nombre9'],$_POST['apellidos9'],$_POST['documento9']);
+
+function NuevoProducto9($tecnologico,$rfc9,$nombre9,$apellidos9,$documento9)
+{
+
+    include 'conexion.php';
+
+    $sentencia= "INSERT INTO `fut_femenil` (`tecnologico`,`rfc_ff`, `nombre`, `apellidos`, `adjunto_ff`) 
+    VALUES ('".$tecnologico."','".$rfc9."','".$nombre9."','".$apellidos9."','".$documento9."') ";
+    $conexion->query($sentencia) or die ("Error al ingresar los datos".mysqli_error($conexion));
+
+}
+NuevoProducto10($_POST['tecnologico'],$_POST['rfc10'], $_POST['nombre10'],$_POST['apellidos10'],$_POST['documento10']);
+
+function NuevoProducto10($tecnologico,$rfc10,$nombre10,$apellidos10,$documento10)
+{
+
+    include 'conexion.php';
+
+    $sentencia= "INSERT INTO `fut_femenil` (`tecnologico`,`rfc_ff`, `nombre`, `apellidos`, `adjunto_ff`) 
+    VALUES ('".$tecnologico."','".$rfc10."','".$nombre10."','".$apellidos10."','".$documento10."') ";
+    $conexion->query($sentencia) or die ("Error al ingresar los datos".mysqli_error($conexion));
+
+}
+NuevoProducto11($_POST['tecnologico'],$_POST['rfc11'], $_POST['nombre11'],$_POST['apellidos11'],$_POST['documento11']);
+
+function NuevoProducto11($tecnologico,$rfc11,$nombre11,$apellidos11,$documento11)
+{
+
+    include 'conexion.php';
+
+    $sentencia= "INSERT INTO `fut_femenil` (`tecnologico`,`rfc_ff`, `nombre`, `apellidos`, `adjunto_ff`) 
+    VALUES ('".$tecnologico."','".$rfc11."','".$nombre11."','".$apellidos11."','".$documento11."') ";
+    $conexion->query($sentencia) or die ("Error al ingresar los datos".mysqli_error($conexion));
+
+}
+NuevoProducto12($_POST['tecnologico'],$_POST['rfc12'], $_POST['nombre12'],$_POST['apellidos12'],$_POST['documento12']);
+
+function NuevoProducto12($tecnologico,$rfc12,$nombre12,$apellidos12,$documento12)
+{
+
+    include 'conexion.php';
+
+    $sentencia= "INSERT INTO `fut_femenil` (`tecnologico`,`rfc_ff`, `nombre`, `apellidos`, `adjunto_ff`) 
+    VALUES ('".$tecnologico."','".$rfc12."','".$nombre12."','".$apellidos12."','".$documento12."') ";
+    $conexion->query($sentencia) or die ("Error al ingresar los datos".mysqli_error($conexion));
+
+}
+NuevoProducto13($_POST['tecnologico'],$_POST['rfc13'], $_POST['nombre13'],$_POST['apellidos13'],$_POST['documento13']);
+
+function NuevoProducto13($tecnologico,$rfc13,$nombre13,$apellidos13,$documento13)
+{
+
+    include 'conexion.php';
+
+    $sentencia= "INSERT INTO `fut_femenil` (`tecnologico`,`rfc_ff`, `nombre`, `apellidos`, `adjunto_ff`) 
+    VALUES ('".$tecnologico."','".$rfc13."','".$nombre13."','".$apellidos13."','".$documento13."') ";
+    $conexion->query($sentencia) or die ("Error al ingresar los datos".mysqli_error($conexion));
+
+}
+NuevoProducto14($_POST['tecnologico'],$_POST['rfc14'], $_POST['nombre14'],$_POST['apellidos14'],$_POST['documento14']);
+
+function NuevoProducto14($tecnologico,$rfc14,$nombre14,$apellidos14,$documento14)
+{
+
+    include 'conexion.php';
+
+    $sentencia= "INSERT INTO `fut_femenil` (`tecnologico`,`rfc_ff`, `nombre`, `apellidos`, `adjunto_ff`) 
+    VALUES ('".$tecnologico."','".$rfc14."','".$nombre14."','".$apellidos14."','".$documento14."') ";
+    $conexion->query($sentencia) or die ("Error al ingresar los datos".mysqli_error($conexion));
+
+}
+NuevoProducto15($_POST['tecnologico'],$_POST['rfc15'], $_POST['nombre15'],$_POST['apellidos15'],$_POST['documento15']);
+
+function NuevoProducto15($tecnologico,$rfc15,$nombre15,$apellidos15,$documento15)
+{
+
+    include 'conexion.php';
+
+    $sentencia= "INSERT INTO `fut_femenil` (`tecnologico`,`rfc_ff`, `nombre`, `apellidos`, `adjunto_ff`) 
+    VALUES ('".$tecnologico."','".$rfc15."','".$nombre15."','".$apellidos15."','".$documento15."') ";
+    $conexion->query($sentencia) or die ("Error al ingresar los datos".mysqli_error($conexion));
+
+}
+NuevoProducto16($_POST['tecnologico'],$_POST['rfc16'], $_POST['nombre16'],$_POST['apellidos16'],$_POST['documento16']);
+
+function NuevoProducto16($tecnologico,$rfc16,$nombre16,$apellidos16,$documento16)
+{
+
+    include 'conexion.php';
+
+    $sentencia= "INSERT INTO `fut_femenil` (`tecnologico`,`rfc_ff`, `nombre`, `apellidos`, `adjunto_ff`) 
+    VALUES ('".$tecnologico."','".$rfc16."','".$nombre16."','".$apellidos16."','".$documento16."') ";
+    $conexion->query($sentencia) or die ("Error al ingresar los datos".mysqli_error($conexion));
+
+}
+NuevoProducto17($_POST['tecnologico'],$_POST['rfc17'], $_POST['nombre17'],$_POST['apellidos17'],$_POST['documento17']);
+
+function NuevoProducto17($tecnologico,$rfc17,$nombre17,$apellidos17,$documento17)
+{
+
+    include 'conexion.php';
+
+    $sentencia= "INSERT INTO `fut_femenil` (`tecnologico`,`rfc_ff`, `nombre`, `apellidos`, `adjunto_ff`) 
+    VALUES ('".$tecnologico."','".$rfc17."','".$nombre17."','".$apellidos17."','".$documento17."') ";
+    $conexion->query($sentencia) or die ("Error al ingresar los datos".mysqli_error($conexion));
+
+}
+NuevoProducto18($_POST['tecnologico'],$_POST['rfc18'], $_POST['nombre18'],$_POST['apellidos18'],$_POST['documento18']);
+
+function NuevoProducto18($tecnologico,$rfc18,$nombre18,$apellidos18,$documento18)
+{
+
+    include 'conexion.php';
+
+    $sentencia= "INSERT INTO `fut_femenil` (`tecnologico`,`rfc_ff`, `nombre`, `apellidos`, `adjunto_ff`) 
+    VALUES ('".$tecnologico."','".$rfc18."','".$nombre18."','".$apellidos18."','".$documento18."') ";
+    $conexion->query($sentencia) or die ("Error al ingresar los datos".mysqli_error($conexion));
+
+}
+NuevoProducto19($_POST['tecnologico'],$_POST['rfc19'], $_POST['nombre19'],$_POST['apellidos19'],$_POST['documento19']);
+
+function NuevoProducto19($tecnologico,$rfc19,$nombre19,$apellidos19,$documento19)
+{
+
+    include 'conexion.php';
+
+    $sentencia= "INSERT INTO `fut_femenil` (`tecnologico`,`rfc_ff`, `nombre`, `apellidos`, `adjunto_ff`) 
+    VALUES ('".$tecnologico."','".$rfc19."','".$nombre19."','".$apellidos19."','".$documento19."') ";
+    $conexion->query($sentencia) or die ("Error al ingresar los datos".mysqli_error($conexion));
+
+}
+NuevoProducto20($_POST['tecnologico'],$_POST['rfc20'], $_POST['nombre20'],$_POST['apellidos20'],$_POST['documento20']);
+
+function NuevoProducto20($tecnologico,$rfc20,$nombre20,$apellidos20,$documento20)
+{
+
+    include 'conexion.php';
+
+    $sentencia= "INSERT INTO `fut_femenil` (`tecnologico`,`rfc_ff`, `nombre`, `apellidos`, `adjunto_ff`) 
+    VALUES ('".$tecnologico."','".$rfc20."','".$nombre20."','".$apellidos20."','".$documento20."') ";
+    $conexion->query($sentencia) or die ("Error al ingresar los datos".mysqli_error($conexion));
+
+}
+NuevoProducto21($_POST['tecnologico'],$_POST['rfc21'], $_POST['nombre21'],$_POST['apellidos21'],$_POST['documento21']);
+
+function NuevoProducto21($tecnologico,$rfc21,$nombre21,$apellidos21,$documento21)
+{
+
+    include 'conexion.php';
+
+    $sentencia= "INSERT INTO `fut_femenil` (`tecnologico`,`rfc_ff`, `nombre`, `apellidos`, `adjunto_ff`) 
+    VALUES ('".$tecnologico."','".$rfc21."','".$nombre21."','".$apellidos21."','".$documento21."') ";
+    $conexion->query($sentencia) or die ("Error al ingresar los datos".mysqli_error($conexion));
+
+}
+NuevoProducto22($_POST['tecnologico'],$_POST['rfc22'], $_POST['nombre22'],$_POST['apellidos22'],$_POST['documento22']);
+
+function NuevoProducto22($tecnologico,$rfc22,$nombre22,$apellidos22,$documento22)
+{
+
+    include 'conexion.php';
+
+    $sentencia= "INSERT INTO `fut_femenil` (`tecnologico`,`rfc_ff`, `nombre`, `apellidos`, `adjunto_ff`) 
+    VALUES ('".$tecnologico."','".$rfc22."','".$nombre22."','".$apellidos22."','".$documento22."') ";
+    $conexion->query($sentencia) or die ("Error al ingresar los datos".mysqli_error($conexion));
+
+}
+
+?>
+
+<script type="text/javascript">
+alert("Ingresado Exitosamante!!");
+window.location.href='feme_cate.html';
+</script>
